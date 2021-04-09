ba593c7b95e42550b8941b8bb6f30cfdd09ed6c9
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-04-09 01:35:20 -0300

    Script que gera o gif do resultado.

---
+++ plotRes/movie/movie.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import sys
+from matplotlib.animation import FuncAnimation
+
+def init():
+    return line, annotation
+
+
+def animate(i):
+    tmp  = lines[i].split()
+    t    = tmp[1]
+    annotation.set_text(f"t = {t} s")
+    temp = [float(j) for j in tmp[2:]]  
+    line.set_data(x, temp)
+    
+    return line, annotation
+ 
+
+nameFile = "../../Python/prob_node.python"
+
+with open(nameFile,'r') as f:
+    data = f.read()
+        
+lines = data.splitlines()
+
+x = lines[0].split()[2:]
+x = [float(i) for i in x]
+
+# valores min e max para um float
+temp_min = sys.float_info[0]
+temp_max = sys.float_info[3]
+
+x_max = max(x)
+x_min = min(x)
+
+time_steps = len(lines) - 1
+
+for i in range(1,time_steps - 1):
+    tmp  = lines[i].split()
+    temp = [float(j) for j in tmp[2:]]    
+    # pegando o valor max e mim de temperatura
+    temp_max = max(max(temp),temp_max)
+    temp_min = min(min(temp),temp_min)
+        
+    
+    
+fig = plt.figure()
+ax = plt.axes(xlim=(x_min, x_max), ylim = (temp_min, temp_max))
+annotation = ax.annotate('t = 0.0 s', xy=(50, 200), xycoords="axes points")
+annotation.set_animated(True)
+
+line, = ax.plot([], [])
+plt.title('Temperatura ao longo da barra')
+plt.xlabel('x (m)')
+plt.ylabel('Temperatua(°C)')    
+
+
+#anim = FuncAnimation(fig, animate, frames = None, init_func=init, interval = 1, save_count=time_steps)
+anim = FuncAnimation(fig, animate, frames = None, init_func=init, interval = 10, save_count=200)
+ 
+
+anim.save('movie.gif')
+ 
+
