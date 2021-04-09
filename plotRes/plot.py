a0ca5bcb76e0fc0ce6eeb3655b51a8ce91d4daa0
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-04-08 23:34:01 -0300

    Arquivo de dados add. Mudancao no arquivo script de plotagem.

--- plotRes/plot.py
+++ plotRes/plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import sys
+
 
 with open('plot.in','r') as f:
     data = f.read()
@@ -20,11 +22,22 @@ lines = data.splitlines()
 x = lines[0].split()[2:]
 x = [float(i) for i in x]
 
+# valores min e max para um float
+temp_min = sys.float_info[0]
+temp_max = sys.float_info[3]
+
 for i in step:
     tmp  = lines[int(i)+1].split()
     t    = tmp[1]
     temp = [float(i) for i in tmp[2:]]
     plt.plot(x,temp,label='t(s) = ' + t) 
+    
+    # pegando o valor max e mim de temperatura
+    temp_max = max(max(temp),temp_max)
+    temp_min = min(min(temp),temp_min)
+
+plt.xlim(min(x) - 1, max(x) + 1)
+plt.ylim(temp_min-1, temp_max+1)
 
 plt.xlabel('time (s)')
 plt.ylabel('Temperatua(°C)')
